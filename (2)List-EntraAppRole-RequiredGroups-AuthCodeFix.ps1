28ce28a98ae58cc97bd4d9a21d5e541b525d8575
valar12 <andrew.lazar@gmail.com>
2026-01-12 11:46:34 -0500

    Add files via upload

--- (2)List-EntraAppRole-RequiredGroups-AuthCodeFix.ps1
+++ (2)List-EntraAppRole-RequiredGroups-AuthCodeFix.ps1
@@ -1,39 +1,61 @@
 Connect-MgGraph -Scopes "Application.Read.All","Directory.Read.All" -NoWelcome
-$results = foreach ($appId in $targetApps.Keys) {
 
-    $sp = Get-MgServicePrincipal `
-        -Filter "appId eq '$appId'" `
-        -ConsistencyLevel eventual `
-        -All `
-        -ErrorAction SilentlyContinue |
-        Select-Object -First 1
+$appIds = @(
+  "04b07795-8ddb-461a-bbee-02f9e1bf7b46",  # Microsoft Azure CLI
+  "1950a258-227b-4e31-a9cf-717495945fc2",  # Microsoft Azure PowerShell
+  "04f0c124-f2bc-4f59-8241-bf6df9866bbd",  # Visual Studio
+  "aebc6443-996d-45c2-90f0-388ff96faa56",  # Visual Studio Code
+  "12128f48-ec9e-42f0-b203-ea49fb6af367"   # MS Teams Powershell Cmdlets
+)
 
-    if (-not $sp) {
-        [PSCustomObject]@{
-            AppName         = $targetApps[$appId]
-            AppId           = $appId
-            SP_DisplayName  = $null
-            SP_ObjectId     = $null
-            RequiredGroups  = "<SP not found>"
-        }
-        continue
+$results = foreach ($appId in $appIds) {
+
+  $sp = Get-MgServicePrincipal -Filter "appId eq '$appId'" -Top 1 -ErrorAction Stop
+
+  # Map roleId(string) -> role display name (if any)
+  $roleMap = @{}
+  foreach ($r in @($sp.AppRoles)) {
+    if ($null -ne $r.Id) { $roleMap[$r.Id.ToString()] = $r.DisplayName }
+  }
+
+  $assignedTo = Get-MgServicePrincipalAppRoleAssignedTo -ServicePrincipalId $sp.Id -All -ErrorAction Stop
+
+  if (-not $assignedTo -or $assignedTo.Count -eq 0) {
+    [PSCustomObject]@{
+      ResourceDisplayName = $sp.DisplayName
+      ResourceAppId       = $sp.AppId
+      ResourceSpId        = $sp.Id
+      AssignmentRequired  = $sp.AppRoleAssignmentRequired
+      PrincipalType       = "<none>"
+      PrincipalDisplayName= "<No assignments found>"
+      PrincipalId         = $null
+      AssignedRole        = $null
+      AssignedRoleId      = $null
     }
+    continue
+  }
 
-    # App role assignments TO this service principal (who has access)
-    $assignedTo = Get-MgServicePrincipalAppRoleAssignedTo -ServicePrincipalId $sp.Id -All -ErrorAction SilentlyContinue
+  foreach ($a in ($assignedTo | Sort-Object PrincipalType, PrincipalDisplayName)) {
 
-    $groups = $assignedTo |
-        Where-Object { $_.PrincipalType -eq "Group" } |
-        Sort-Object PrincipalDisplayName |
-        Select-Object -ExpandProperty PrincipalDisplayName -Unique
+    $roleIdStr = if ($null -ne $a.AppRoleId) { $a.AppRoleId.ToString() } else { $null }
+
+    $roleName =
+      if ([string]::IsNullOrWhiteSpace($roleIdStr)) { "<no app role>" }
+      elseif ($roleMap.ContainsKey($roleIdStr))     { $roleMap[$roleIdStr] }
+      else                                          { "<role id not in appRoles>" }
 
     [PSCustomObject]@{
-        AppName         = $targetApps[$appId]
-        AppId           = $appId
-        SP_DisplayName  = $sp.DisplayName
-        SP_ObjectId     = $sp.Id
-        RequiredGroups  = if ($groups) { $groups -join "; " } else { "<No group assigned>" }
+      ResourceDisplayName  = $sp.DisplayName
+      ResourceAppId        = $sp.AppId
+      ResourceSpId         = $sp.Id
+      AssignmentRequired   = $sp.AppRoleAssignmentRequired
+      PrincipalType        = $a.PrincipalType
+      PrincipalDisplayName = $a.PrincipalDisplayName
+      PrincipalId          = $a.PrincipalId
+      AssignedRole         = $roleName
+      AssignedRoleId       = $a.AppRoleId
     }
+  }
 }
 
 $results | Format-Table -AutoSize
